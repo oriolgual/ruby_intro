3eaabf6fbe69835c753e200b4ef07aa5ead52ece
Oriol Gual <oriol.gual@gmail.com>
2014-02-25 15:34:01 +0100

    Describe reviews

--- spec/lib/movie_spec.rb
+++ spec/lib/movie_spec.rb
@@ -31,4 +31,45 @@ describe Movie do
       expect(ordered_movies.last).to eq(top_gun)
     end
   end
+
+  describe 'rating' do
+    let('movie') do
+      Movie.new('Die Hard', 'Action', Date.new(1996, 1, 1))
+    end
+
+    it 'calculates the movie rating based on reviews' do
+      great = Review.new('Great movie', 'Great!!', 5)
+      meh = Review.new('Meeehhh', 'Not impressive', 3)
+      bad = Review.new('This movie sucks', "Don't watch it!", 0)
+
+      movie.reviews << great
+      movie.reviews << meh
+      movie.reviews << bad
+
+      expect(movie.rating).to eq(3)
+    end
+
+    context "when it has no reviews" do
+      it 'returns nil' do
+        expect(movie.rating).to eq(nil)
+      end
+    end
+  end
+
+  describe 'reviews' do
+    it 'returns an array of reviews' do
+      great = Review.new('Great movie', 'Great!!', 5)
+      meh = Review.new('Meeehhh', 'Not impressive', 3)
+      bad = Review.new('This movie sucks', "Don't watch it!", 0)
+
+      movie.reviews << great
+      movie.reviews << meh
+      movie.reviews << bad
+
+      expect(movie.reviews.length).to eq(3)
+      expect(movie.reviews[0]).to eq(great)
+      expect(movie.reviews[1]).to eq(meh)
+      expect(movie.reviews[2]).to eq(bad)
+    end
+  end
 end
--- spec/lib/review_spec.rb
+++ spec/lib/review_spec.rb
@@ -2,5 +2,21 @@ require 'spec_helper'
 require 'review'
 
 describe Review do
+  describe 'attributes' do
+    let(:revire) do
+      Review.new('Great movie', 'Lorem ipsum dolor sir amet consecutum', 4)
+    end
 
+    it 'has a title' do
+      expect(review.title).to eq('Great movie')
+    end
+
+    it 'has a text' do
+      expect(review.title).to eq('Lorem ipsum dolor sit amet consecutum')
+    end
+
+    it 'has a rating' do
+      expect(review.rating).to eq(4)
+    end
+  end
 end
