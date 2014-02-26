998a6765bb0960907cffdce6766f4c0e59512770
Oriol Gual <oriol.gual@gmail.com>
2014-02-26 11:10:16 +0100

    Add similar movie to a cinema

--- lib/cinema.rb
+++ lib/cinema.rb
@@ -7,6 +7,14 @@ class Cinema
     @movies.sort.map{ |movie| movie.title }
   end
 
+  def add_movie(movie)
+    @movies.push(movie)
+  end
+
+  def remove_movie(movie_to_remove)
+    @movies.delete(movie_to_remove)
+  end
+
   def listing_by_genre(genre)
     movies_by_genre = @movies.select do |movie|
       movie.genre == genre
@@ -25,14 +33,6 @@ class Cinema
     end.sort.map{|movie| movie.title}
   end
 
-  def add(movie)
-    @movies.push(movie)
-  end
-
-  def remove(movie_to_remove)
-    @movies.delete(movie_to_remove)
-  end
-
   def group_by_genre
     @movies.reduce({}) do |movies_by_genre, movie|
       movies_by_genre[movie.genre] ||= []
@@ -46,6 +46,20 @@ class Cinema
       movies_per_genre[movie.genre] ||= 0
       movies_per_genre[movie.genre] += 1
       movies_per_genre
+    end
+  end
+
+  def similar_movies(movie_to_compare)
+    similar_movies = @movies.map do |movie|
+      [movie_to_compare.similarity_to(movie), movie]
+    end
+
+    similar_movies.select! do |similarity, movie|
+      similarity > 20
+    end
+
+    similar_movies.map do |similarity, movie|
+      movie.title
     end
   end
 end
--- spec/lib/cinema_spec.rb
+++ spec/lib/cinema_spec.rb
@@ -1,14 +1,26 @@
 require 'spec_helper'
 require 'cinema'
+require 'movie'
 
 describe Cinema do
   describe 'add' do
     it 'adds a movie to our listing' do
       cinema = Cinema.new
 
-      cinema.add(Movie.new('Top Gun', 'Action', 'Tony Scott', Date.new(1986, 5, 16)))
+      cinema.add_movie(Movie.new('Top Gun', 'Action', 'Tony Scott', Date.new(1986, 5, 16)))
 
       expect(cinema.listing).to include('Top Gun')
+    end
+  end
+
+  describe 'remove' do
+    it 'remove a movie from our listing' do
+      top_gun = Movie.new('Top Gun', 'Action', 'Tony Scott', Date.new(1986, 5, 16))
+      cinema = Cinema.new([top_gun])
+
+      cinema.remove_movie(top_gun)
+
+      expect(cinema.listing).to_not include('Top Gun')
     end
   end
 
@@ -68,6 +80,24 @@ describe Cinema do
       expect(histogram['Action']).to eq(1)
       expect(histogram['Animation']).to eq(2)
       expect(histogram['Comedy']).to eq(1)
+    end
+  end
+
+  describe 'similar_movies' do
+    it 'returns the titles of similar movies' do
+      die_hard = Movie.new('Die Hard', 'Action', 'John McTiernan', Date.new(1988, 7, 22))
+      aladdin = Movie.new('Aladdin', 'Animation', 'Ron Clements', Date.new(1992, 11, 25))
+      aladdin2 = Movie.new('Aladdin 2', 'Animation', 'Toby Shelton', Date.new(1994, 5, 20))
+      clerks = Movie.new('Clerks', 'Comedy', 'Kevin Smith', Date.new(1994, 11, 30))
+      cinema = Cinema.new([die_hard, aladdin, aladdin2, clerks])
+
+      up = Movie.new('Up', 'Animation', 'Pete Docter', Date.new(2009, 5, 29))
+
+      similar_movies = cinema.similar_movies(up)
+
+      expect(similar_movies).to include('Aladdin')
+      expect(similar_movies).to include('Aladdin 2')
+      expect(similar_movies.length).to eq(2)
     end
   end
 end
